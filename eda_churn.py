eacfac0f40c006c13e442c06dd9e955dd1b54db3
LeenaKH123 <engleena@yahoo.com>
2025-07-20 15:12:01 +0300

    test

--- eda_churn.py
+++ eda_churn.py
@@ -1,4 +1,26 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+# Load the data set
 df = pd.read_csv("Telco-Customer-Churn.csv")
+# Clean the totalCharges column
+df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors='coerce')
+df.dropna(inplace=True)
+# Drop non-informative ID column
+df.drop(columns=['customerID'], inplace=True)
+# Convert Churn to Binary
+df['Churn'] = df['Churn'].map({'Yes': 1, 'No': 0})
+# EDA 1: Churn distribution by tenure
+sns.histplot(data=df, x='tenure', hue='Churn', kde=True)
+plt.title('Churn Distribution by Tenure')
+plt.xlabel('Tenure (Months)')
+plt.ylabel('Number of Customers')
+plt.savefig("churn_tenure.png")
+plt.show()
+# EDA 2: Churn by contract type
+sns.countplot(data=df, x='Contract', hue='Churn')
+plt.title('Churn by Contract Type')
+plt.xticks(rotation=15)
+plt.tight_layout()
+plt.savefig("churn_contract.png")
+plt.show()
